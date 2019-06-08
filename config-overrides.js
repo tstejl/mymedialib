6427b83480eb03c201ad1eeb4d2e6130802d0d7e
tstejl <jameslex19951030@gmail.com>
2019-06-09 01:44:54 +0800

    initial commit

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+import { override, fixBabelImports, addLessLoader } from 'customize-cra';
+
+export default override(
+  fixBabelImports('import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: true,
+  }),
+  addLessLoader({
+    javascriptEnabled: true,
+    modifyVars: { '@primary-color': '#1DA57A' },
+  }),
+);
